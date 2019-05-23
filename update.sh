666384faaf78e913c3a8413f9c3233660ff79d3d
Daichi Hioki <hiokidaichi@gmail.com>
2019-05-23 13:36:47 +0900

    Also update the enabled rules in Karabiner-Elements in update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-ruby personal_rules.rb | jq . > personal_rules.json \
-  && cp personal_rules.json ~/.config/karabiner/assets/complex_modifications/
+SRC_FILE=./personal_rules.rb
+DST_FILE=./personal_rules.json
+
+ruby $SRC_FILE | jq . > $DST_FILE && cp $DST_FILE ~/.config/karabiner/assets/complex_modifications/
+
+TARGET="$HOME/.config/karabiner/karabiner.json"
+QUERY=".profiles[0].complex_modifications.rules = $(jq '.rules' $DST_FILE)"
+
+cat <<< "$(jq "$QUERY" < $TARGET)" > $TARGET
