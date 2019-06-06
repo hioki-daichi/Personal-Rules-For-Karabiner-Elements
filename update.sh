a3bc4bfb25b7fc3ba3b6c048c2b7f898c1fe7cfa
Daichi Hioki <hiokidaichi@gmail.com>
2019-06-06 14:57:00 +0900

    Introduce check process of jq command and config directory

--- update.sh
+++ update.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-SRC_FILE=./personal_rules.rb
-DST_FILE=./personal_rules.json
+CONFIG_DIR="$HOME/.config/karabiner"
+SRC_FILE="./personal_rules.rb"
+DST_FILE="./personal_rules.json"
 
-ruby $SRC_FILE | jq . > $DST_FILE && cp $DST_FILE ~/.config/karabiner/assets/complex_modifications/
+if ! type jq >/dev/null 2>&1; then
+  echo "jq command is required." 1>&2
+  exit 1
+fi
 
-TARGET="$HOME/.config/karabiner/karabiner.json"
+if [ ! -d ${CONFIG_DIR} ]; then
+  echo "Create directory ${CONFIG_DIR} by operating Karabiner-Elements with GUI" 1>&2
+  exit 2
+fi
+
+ruby $SRC_FILE | jq . > $DST_FILE && cp $DST_FILE "${CONFIG_DIR}/assets/complex_modifications/"
+
+TARGET="${CONFIG_DIR}/karabiner.json"
 QUERY=".profiles[0].complex_modifications.rules = $(jq '.rules' $DST_FILE)"
 
 cat <<< "$(jq "$QUERY" < $TARGET)" > $TARGET
